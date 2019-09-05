c7ec177cfb154696c8079766663066850863cb21
Omer Latif <olatif@u.rochester.edu>
2019-09-05 12:58:24 -0400

    Added bootstrap

--- CommunityShed/CommunityShed/Site.Master.designer.cs
+++ CommunityShed/CommunityShed/Site.Master.designer.cs
@@ -22,6 +22,24 @@ namespace CommunityShed {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// HomeHyperLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HomeHyperLink;
+        
+        /// <summary>
+        /// PersonAddHyperLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink PersonAddHyperLink;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
